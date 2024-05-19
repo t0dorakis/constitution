4a59a4154703a2e19ef7d1b6b8bff43b13545b0a
t0dorakis <theodor.hillmann@funkemedien.de>
2024-05-19 17:23:59 +0200

    feature: navbar, custom fonts and hover animations

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupTextExplorer } from "./components/textExplorer";
+import { setupNavBar } from "./components/navBar";
 
 document.querySelector("#app").innerHTML = `
-  <div class="container">
+  <main>
+    <div id="nav_bar"></div>
     <div id="text_explorer"></div>
-  </div>
+  </main>
 `;
 setupTextExplorer(document.querySelector("#text_explorer"));
+setupNavBar(document.querySelector("#nav_bar"));
