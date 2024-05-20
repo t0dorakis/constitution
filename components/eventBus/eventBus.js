c58ed15ded7189cb6cefb7497ab02130ac4e56a9
t0dorakis <theodor.hillmann@funkemedien.de>
2024-05-20 20:04:07 +0200

    added event Bus but probably a simple state is better

---
+++ components/eventBus/eventBus.js
@@ -0,0 +1,27 @@
+export const events = {
+  highlightHuman: "highlight-human",
+  highlightAi: "highlight-ai",
+};
+
+/**
+ * Dispatches an event with the given type and data
+ * @param {string} type
+ * @param {any} data
+ */
+export const dispatchEvent = (type, data) => {
+  window.dispatchEvent(
+    new CustomEvent(type, {
+      detail: data,
+    })
+  );
+};
+
+/**
+ * Subscribes to an event and calls the callback function when the event is triggered
+ * @param {string} eventType
+ * @param {function} callback
+ * @returns {void}
+ */
+export const subscribeToEvent = (eventType, callback) => {
+  window.addEventListener(eventType, callback);
+};
