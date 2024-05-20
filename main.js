42889758b7387e69dcd10153650dafe5420d63cc
t0dorakis <theodor.hillmann@funkemedien.de>
2024-05-20 20:01:55 +0200

    added state mamager and reinitiatet 3d for fun

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ import javascriptLogo from "./javascript.svg";
 import viteLogo from "/vite.svg";
 import { setupTextExplorer } from "./components/textExplorer";
 import { setupNavBar } from "./components/navBar";
+import { setupThreeCanvas } from "./components/threeCanvas";
 
 document.querySelector("#app").innerHTML = `
   <main>
     <div id="nav_bar"></div>
     <div id="text_explorer"></div>
+    <div id="three_canvas"></div>
   </main>
 `;
 setupTextExplorer(document.querySelector("#text_explorer"));
 setupNavBar(document.querySelector("#nav_bar"));
+setupThreeCanvas(document.querySelector("#three_canvas"));
